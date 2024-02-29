0226747e0f90ecf486862c4ff02ce0e7a54573b6
eorafscod <rafael.nobre.df@gmail.com>
2024-02-29 20:12:51 -0300

    adicionando mais arquivos

--- JS.js
+++ JS.js
@@ -1,1 +1,8 @@
+// imprime um popup
 alert("salve familia, eorafs aqui");
+
+// imprime diretamente na pagina do navegador
+document.write("boa tarde")
+
+// imprime na console do navegador, na ferramenta
+console.log("boa noite")
